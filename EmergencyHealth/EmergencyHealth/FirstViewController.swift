ca511c4b84937433a71a06a35f2405a7db46d41f
Sam <Sam@dhcp-hol-1193.redrover.cornell.edu>
2017-11-15 22:41:04 -0500

    Starter Code for iOS App

---
+++ EmergencyHealth/EmergencyHealth/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EmergencyHealth
+//
+//  Created by Sam on 11/15/17.
+//  Copyright © 2017 EmergencyHealthServices. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
